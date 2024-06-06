b89482992f93c3b542882adf4755a8d04395fda9
leleliu008 <leleliu008@gmail.com>
2024-06-06 14:29:11 +0800

    optimized

--- wrapper-native-cc.c
+++ wrapper-native-cc.c
@@ -4,37 +4,115 @@
 #include <unistd.h>
 
 int main(int argc, char * argv[]) {
-    char * const cc = getenv("PROXIED_CC_FOR_BUILD");
+    char * const compiler = getenv("XCPKG_COMPILER_C");
 
-    if (cc == NULL) {
-        fprintf(stderr, "PROXIED_CC_FOR_BUILD environment variable is not set.\n");
+    if (compiler == NULL) {
+        fprintf(stderr, "XCPKG_COMPILER_C environment variable is not set.\n");
         return 1;
     }
 
-    if (cc[0] == '\0') {
-        fprintf(stderr, "PROXIED_CC_FOR_BUILD environment variable value should be a non-empty string.\n");
+    if (compiler[0] == '\0') {
+        fprintf(stderr, "XCPKG_COMPILER_C environment variable value should be a non-empty string.\n");
         return 2;
     }
 
-    char* argv2[argc + 2];
+    /////////////////////////////////////////////////////////////////
 
-    argv2[0] = cc;
+    char * const baseArgs = getenv("XCPKG_COMPILER_ARGS_FOR_BUILD");
+
+    if (baseArgs == NULL) {
+        fprintf(stderr, "XCPKG_COMPILER_ARGS_FOR_BUILD environment variable is not set.\n");
+        return 5;
+    }
+
+    if (baseArgs[0] == '\0') {
+        fprintf(stderr, "XCPKG_COMPILER_ARGS_FOR_BUILD environment variable value should be a non-empty string.\n");
+        return 6;
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    size_t baseArgc = 1U;
+
+    for (size_t i = 0U; ; i++) {
+        if (baseArgs[i] == '\0') {
+            break;
+        }
+
+        if (baseArgs[i] == ' ') {
+            baseArgc++;
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    int createSharedLibrary = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-dynamiclib") == 0 || strcmp(argv[i], "-shared") == 0) {
+            createSharedLibrary = 1;
+            break;
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    char* argv2[argc + baseArgc + 2];
+
+    argv2[0] = compiler;
 
     for (int i = 1; i < argc; i++) {
         argv2[i] = argv[i];
     }
 
-    char * const sysroot = getenv("SYSROOT_FOR_BUILD");
+    /////////////////////////////////////////////////////////////////
 
-    if (sysroot == NULL || sysroot[0] == '\0') {
-        argv2[argc] = NULL;
-    } else {
-        argv2[argc]     = (char*)"-isysroot";
-        argv2[argc + 1] = sysroot;
-        argv2[argc + 2] = NULL;
+    char * p = baseArgs;
+
+    for (size_t i = 0U; ; i++) {
+        if (baseArgs[i] == '\0') {
+            if (p[0] != '\0') {
+                argv2[argc++] = p;
+            }
+            break;
+        }
+
+        if (baseArgs[i] == ' ') {
+            baseArgs[i] = '\0';
+
+            if (p[0] != '\0') {
+                argv2[argc++] = p;
+            }
+
+            p = &baseArgs[i + 1];
+        }
     }
 
-    execv (cc, argv2);
-    perror(cc);
+    /////////////////////////////////////////////////////////////////
+
+    if (createSharedLibrary == 1) {
+        argv2[argc++] = (char*)"-fPIC";
+    }
+
+    argv2[argc++] = NULL;
+
+    /////////////////////////////////////////////////////////////////
+
+    const char * verbose = getenv("XCPKG_VERBOSE");
+
+    if (verbose != NULL && strcmp(verbose, "1") == 0) {
+        for (int i = 0; ;i++) {
+            if (argv2[i] == NULL) {
+                break;
+            } else {
+                fprintf(stderr, "%s\n", argv2[i]);
+            }
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    execv (compiler, argv2);
+    perror(compiler);
     return 255;
 }
