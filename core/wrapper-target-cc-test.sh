2fffdae339bd874ba66f74b281f5af457a46f037
leleliu008 <leleliu008@gmail.com>
2026-02-03 02:30:32 +0800

    optimized

--- core/wrapper-target-cc-test.sh
+++ core/wrapper-target-cc-test.sh
@@ -7,7 +7,7 @@ NATIVE_PLATFORM_ARCH="$(uname -m)"
 
 SYSROOT="$(xcrun --sdk macosx --show-sdk-path)"
 
-export XCPKG_COMPILER_C="$(xcrun --sdk macosx --find clang)"
+export XCPKG_CC="$(xcrun --sdk macosx --find clang)"
 export XCPKG_COMPILER_ARGS="-isysroot $SYSROOT     -mmacosx-version-min=$NATIVE_PLATFORM_VERS -arch $NATIVE_PLATFORM_ARCH -Qunused-arguments    -ldl"
 export XCPKG_VERBOSE=1
 
